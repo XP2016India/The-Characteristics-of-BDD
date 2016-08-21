c1145f4fa06c480294f38281376009cefd302202
banuprakashurs <banuprakash.urs@gmail.com>
2016-08-21 21:24:56 +0530

    Update CheckOutStepsDefinition.java
    removed static usage of assert

--- src/test/java/com/bddworkshop/shoppingcart/checkout/CheckOutStepsDefinition.java
+++ src/test/java/com/bddworkshop/shoppingcart/checkout/CheckOutStepsDefinition.java
@@ -10,47 +10,49 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import static org.junit.Assert.*;
+
 
 public class CheckOutStepsDefinition {
 	
-	private String user;
-	private String product;
-	private String price;
-	private String vat;
-	private CheckOutService checkOutService = new CheckOutService();
-	CheckOutItem checkOutItem = null; 
-	
-	
-	@Given("^user \"([^\"]*)\" has logged in$")
-	public void user_has_logged_in(String user) throws Throwable {
-		this.user = user; 
-	}
 
-	@Given("^has \"([^\"]*)\" mobile in the cart priced Rs \"([^\"]*)\"$")
-	public void has_mobile_in_the_cart_priced_Rs(String product, String price) throws Throwable {
-	    this.product = product;
-	    this.price = price;
-	}
 
-	@Given("^Vat is \"([^\"]*)\" %$")
-	public void Vat_is_(String vat) throws Throwable {
-	    this.vat = vat;
-	}
+private CheckOutItem checkOutItem;
+private CheckOutService checkOutService = new CheckOutService();
+private String vat;
+private String product;
+private String price;
+private String user;
 
-	@When("^user checks out the cart$")
-	public void user_checks_out_the_cart() throws Throwable {
-	    this.checkOutItem = checkOutService.checkOut(user, product, price, vat);
-	}
+@Given("^user \"([^\"]*)\" has logged in$")
+public void user_has_logged_in(String user) throws Throwable {
+    this.user = user;
+}
 
-	@Then("^confirmation of billing is generated with \"([^\"]*)\" mobile$")
-	public void confirmation_of_billing_is_generated_with_mobile(String productInCheckOut) throws Throwable {
-	    assertEquals(productInCheckOut, checkOutItem.getProduct());
-	}
+@Given("^has \"([^\"]*)\" mobile in the cart priced Rs \"([^\"]*)\"$")
+public void has_mobile_in_the_cart_priced_Rs(String product, String price) throws Throwable {
+	this.product = product;
+	this.price = price;
+}
 
-	@Then("^Total price is Rs \"([^\"]*)\"$")
-	public void Total_price_is_Rs(String totalPriceInCheckOut) throws Throwable {
-		assertEquals(new BigDecimal(totalPriceInCheckOut), checkOutItem.getTotalPrice());
-	}
+@Given("^Vat is \"([^\"]*)\" %$")
+public void Vat_is_(String vat) throws Throwable {
+    this.vat = vat;
+}
+
+@When("^user checks out the cart$")
+public void user_checks_out_the_cart() throws Throwable {
+    this.checkOutItem = this.checkOutService.checkOut(user, product, price, vat);
+}
+
+@Then("^confirmation of billing is generated with \"([^\"]*)\" mobile$")
+public void confirmation_of_billing_is_generated_with_mobile(String productInCheckOut) throws Throwable {
+    Assert.assertEquals(productInCheckOut, checkOutItem.getProduct());
+}
+
+@Then("^Total price is Rs \"([^\"]*)\"$")
+public void Total_price_is_Rs(String totalPriceInCheckOut) throws Throwable {
+	Assert.assertEquals(new BigDecimal(totalPriceInCheckOut), checkOutItem.getTotalPrice());
+}
+
 
 }
